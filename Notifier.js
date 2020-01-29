9b982d0808976f35ca4b8b5d24cf3bb238737399
John D. Allen <john@bigredstack.com>
2020-01-29 10:13:57 -0600

    Initial Commit

---
+++ Notifier.js
@@ -0,0 +1,61 @@
+//
+// Notifier.js  --  show OSX Notifications from MQTT Notification messages.
+//
+// John D. Allen
+// Sept 2016
+// F5 Networks
+//
+// Node-Notifier:  https://github.com/mikaelbr/node-notifier
+//
+
+var mqtt = require('mqtt');
+var notify = require('node-notifier');
+
+var BROKER = "mqtt://10.1.1.28";
+var TOPSPEED = 24.99;
+
+var copts = {
+  clientId: "LT_Notifier",
+  keepalive: 20000
+};
+
+var client = mqtt.connect(BROKER, copts);
+
+client.on('connect', function() {
+  console.log("MQTT Notification daemon Connected...");
+  client.subscribe('alert/#');
+  client.subscribe('wind/speed');
+});
+
+client.on('message', function(topic, msg) {
+  if(topic.indexOf('wind') != -1) {
+    var rr = JSON.parse(msg.toString());
+    if(rr.speed > TOPSPEED) {
+      notify.notify({
+        title: '--ALERT!-- High Winds',
+        message: 'Wind speed of ' + rr.speed + ' MPH was just recorded.',
+        sound: "Sosumi"
+      }, function(err,rsp) {
+        // do nothing...we don't care if it error'd, or there was a response.
+      });
+    }
+  } else {
+    notify.notify({
+      title: '--ALERT!--  ' + topic,
+      message: msg.toString(),
+      sound: "Sosumi"
+    }, function(err,resp) {
+      //'resp' is any response from the notification system.
+    });
+  }
+
+  //var out = topic + ": " + message.toString();
+  //console.log(out);
+});
+
+process.on('SIGINT', function() {     // catch CTRL-C for exiting program
+  console.log('Exiting...');
+  client.unsubscribe('alert/+');
+  client.end();
+  process.exit();
+});
---
+++ README.md
@@ -0,0 +1,6 @@
+#OSX Notifier
+
+Watches for MQTT messages that start with 'alert/' and pushes the message to the OSX Notification Center. This creates a Notification and also playes a sound.  It also watches for "High Winds" from my weather station.
+
+Several sensors in my [[Smart Home|http://diysmarthome.us]] send out alert conditions that are picked up by some of my computers and displayed on the screen.
+
